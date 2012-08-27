f38d40a17f3b4ba76750e827845c31dbfbb88894
Ross Williams <ross.williams@hannonhill.com>
2012-08-27 18:01:02 -0400

    Added basic support for iterating over items.

--- convert.py
+++ convert.py
@@ -31,7 +31,7 @@ class SquareCSVIterator(object):
         return self
     def next(self):
         row = self.trans.reader.next()
-        for k, v in row.iteritems()
+        for k, v in row.iteritems():
             if v[0] == '$':
                 row[k] = Decimal(v[1:])
             if k == 'Date':
@@ -51,15 +51,8 @@ def main():
     transactions = csv.DictReader(transactions_file, dialect='excel')
     items_file = open(os.path.join(PROJECT_ROOT, 'items.csv'), 'r')
     items = csv.DictReader(items_file, dialect='excel')
-
-    output_dialect = csv.Dialect()
-    output_dialect.delimeter = "\t"
-    output_dialect.doublequote = False
-    output_dialect.escapechar = '\\'
-    output_dialect.quotechar = '"'
     
     output_file = open(os.path.join(PROJECT_ROOT, 'output.iif'), 'w')
-    #output = csv.DictWriter(output_file, dialect=output_dialect)
 
     # This is the name of the QuickBooks checking account
     account = "Square"
@@ -73,33 +66,57 @@ def main():
     output_file.write(head)
 
     trans_template = "TRNS		CASH SALE	{month:02d}/{day:02d}/{year:d}	{till_account}	{customer}	{qb_class}	{total:.2f}	{square_id:s}	{cc_digits:s}	N	N\r\n"
-    item_template = "SPL		CASH SALE	{month:02d}/{day:02d}/{year:d}	{sales_account}		{class}	-{total:.2f}			{qty:d}	{price:.2f}	{item_name:s}	N\r\n"
+    item_template = "SPL		CASH SALE	{month:02d}/{day:02d}/{year:d}	{sales_account}		{qb_class}	-{total:.2f}			{qty:d}	{price:.2f}	{item_name:s}	N\r\n"
     trans_footer = "ENDTRNS\r\n"
 
 
+    # TODO: implement config file
+    cfg_cashAccount = 'Market Till'
+    cfg_defaultSalesAccount = 'Sales'
+    cfg_customer = 'PRFM Customers'
+    cfg_defaultClass = 'Layers'
+
+    # Initialize a buffer for stray items
+    itemBuffer = list()
+    
     # And here's the part that inserts data into the tempalate
     for trans in transactions:
-
         try:
-            (date, amount, comments) = list
-        #            date = date.replace('/', '-')
+            (year, month, day) = map(int,trans['Date'].split('-', 2))
         except:
             error(trans)
             continue
-
+        
+        square_id = trans['Payment ID']
+        cc_digits = trans['Card Number'].translate(None,'="')
         try:
-            amount = float(amount)
+            total = float(trans['Total'].lstrip('$'))
         except:
             error(trans)
             continue
-
-        comments = comments.strip('"')
-        comments = comments.strip("\n")
-        comments = comments.strip("\r")
-
-        output_file.write(template % (date, amount, comments, date,
-                                      date, amount, amount))
-
+        
+        output_file.write(trans_template.format(month=month, day=day, year=year, till_account=cfg_cashAccount, customer=cfg_customer, qb_class=cfg_defaultClass, total=total, square_id=square_id, cc_digits=cc_digits))
+        while True:
+            if len(itemBuffer) > 0:
+                item = itemBuffer.pop()
+            else:
+                try:
+                    item = items.next()
+                except StopIteration:
+                    break
+                if item['Payment ID'] != square_id:
+                    itemBuffer.append(item)
+                    break
+            try:
+                item_total = float(item['Price'].lstrip('$')) - float(item['Discount'].lstrip('$'))
+            except:
+                error(item)
+                break
+            
+            # TODO: count quantities
+            output_file.write(item_template.format(month=month, day=day, year=year, sales_account=cfg_defaultSalesAccount, qb_class=cfg_defaultClass, total=item_total, qty=1, price=item_total, item_name=item['Item Name']))
+        
+        output_file.write(trans_footer)
 
 if __name__ == '__main__':
     main()
