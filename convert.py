ae307a670ec004f86e83a2d2ac69ec47d9b613e4
Ross Williams <ross@ross-williams.net>
2012-09-03 20:38:53 -0400

    Got class reading square csv files into sqlite3 tables

--- convert.py
+++ convert.py
@@ -3,57 +3,107 @@
 import os
 import sys, traceback, re
 import csv
-from decimal import *
+import sqlite3
 
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+output_file = open(os.path.join(PROJECT_ROOT, 'output.iif'), 'w')
 
-class SquareTransactions(object):
+class SquareReader(object):
     """Interprets squareup.com CSV export files"""
-    def __init__(self, fh):
-        super(SquareCSVReader, self).__init__()
-        self.fh = fh
-        self.reader = csv.DictReader(self.fh, dialect='excel')
+    TRANS_TEMPLATE = "TRNS		CASH SALE	{month:02d}/{day:02d}/{year:d}	{till_account}	{customer}	{qb_class}	{total:.2f}	{square_id:s}	{cc_digits:s}	N	N\r\n"
+    TRANS_TYPES = {'Subtotal':'REAL','Discount':'REAL','Sales Tax':'REAL','Tips':'REAL','Total':'REAL','Fee':'REAL','Net':'REAL',}
+    ITEM_TEMPLATE = "SPL		CASH SALE	{month:02d}/{day:02d}/{year:d}	{sales_account}		{qb_class}	-{total:.2f}			{qty:d}	{price:.2f}	{item_name:s}	N\r\n"
+    ITEM_TYPES = {'Price':'REAL','Discount':'REAL','Tax':'REAL',}
+    TRANS_FOOTER = "ENDTRNS\r\n"
+
+
+    def __init__(self, transactions_fh, items_fh, deposits_fh=None):
+        self.transactionsFile = transactions_fh
+        self.transactionsReader = csv.reader(self.transactionsFile, dialect='excel')
+        self.transactionsFields = self.transactionsReader.next() 
+        self.itemsFile = items_fh
+        self.itemsReader = csv.reader(self.itemsFile, dialect='excel')
+        self.itemsFields = self.itemsReader.next()
+        if deposits_fh is not None:
+            self.depositsFile = deposits_fh
+            self.depositsReader = csv.DictReader(self.depositsFile, dialect='excel')
+        else:
+            self.depositsFile = None
+            self.depositsReader = None
+
+        # Use tempfile databases
+        self.db = sqlite3.connect('')
+
+        # Create the transactions table
+        transactionFieldsSql = []
+        for field in self.transactionsFields:
+            if field in self.TRANS_TYPES:
+                fieldType = self.TRANS_TYPES[field]	
+            else:
+                fieldType = 'TEXT'
+            fieldSql = '"%s" %s' % (field, fieldType)
+            transactionFieldsSql.append(fieldSql)
+
+        createTransactionsSql = 'CREATE TABLE transactions ( %s )' % ','.join(transactionFieldsSql)
+        self.db.execute(createTransactionsSql)
+
+        # Create the items table
+        itemFieldsSql = []
+        for field in self.itemsFields:
+            if field in self.ITEM_TYPES:
+                fieldType = self.ITEM_TYPES[field]	
+            else:
+                fieldType = 'TEXT'
+            fieldSql = '"%s" %s' % (field, fieldType)
+            itemFieldsSql.append(fieldSql)
+
+        createItemsSql = 'CREATE TABLE items ( %s )' % ','.join(itemFieldsSql)
+        self.db.execute(createItemsSql)
+
+        self.db.commit()
+
+    def importTransactions(self):
+        transactionsInsertSql = 'INSERT INTO transactions VALUES (%s);' % ( ('?, ' * len(self.transactionsFields)).rstrip(', ') )
+        cur = self.db.cursor()
+        cur.executemany(transactionsInsertSql, self.transactionsReader)
         
-    def __iter__(self):
-        return SquareCSVIterator(self)
+    def importItems(self):
+        itemsInsertSql = 'INSERT INTO items VALUES (%s);' % ( ('?, ' * len(self.itemsFields)).rstrip(', ') )
+        cur = self.db.cursor()
+        cur.executemany(itemsInsertSql, self.itemsReader)
         
-    def dumpAll(self):
+    def dumpSqliteMaster(self):
+        cur = self.db.cursor()
+        cur.execute('SELECT * FROM sqlite_master;')
+        print repr(cur.fetchall())
+
+    def dumpSql(self):
+        for line in self.db.iterdump():
+            print line
+
+    def dumpCsv(self):
         try:
-            for row in self.reader:
+            for row in self.transactionsReader:
                 print row
         except csv.Error, e:
-            sys.exit('file %s, line %d: %s' % (self.fh.name, self.reader.line_num, e))
+            sys.exit('file %s, line %d: %s' % (self.transactionsFile.name, self.transactionsReader.line_num, e))
 
-class SquareCSVIterator(object):
-    def __init__(self, transactions):
-        self.trans = transactions
-    def __iter__(self):
-        return self
-    def next(self):
-        row = self.trans.reader.next()
-        for k, v in row.iteritems():
-            if v[0] == '$':
-                row[k] = Decimal(v[1:])
-            if k == 'Date':
-                pass
-            if k == 'Time':
-                pass
-        return row
-                
-        
 def error(trans):
     sys.stderr.write("%s\n" % trans)
     traceback.print_exc(None, sys.stderr)
 
+def outputItem(item, quantity, account, qb_class):
+    item_template = "SPL		CASH SALE	{month:02d}/{day:02d}/{year:d}	{sales_account}		{qb_class}	-{total:.2f}			{qty:d}	{price:.2f}	{item_name:s}	N\r\n"
+    item_total = float(item['Price'].lstrip('$')) - float(item['Discount'].lstrip('$')) * quantity
+    output_file.write(item_template.format(month=month, day=day, year=year, sales_account=account, qb_class=qb_class, total=item_total, qty=quantity, price=item_total, item_name=item['Item Name']))
+    print "------ ITEM %d x %r Written ------" % (quantity,item['Item Name'])
 
 def main():
     transactions_file = open(os.path.join(PROJECT_ROOT, 'transactions.csv'), 'r')
-    transactions = csv.DictReader(transactions_file, dialect='excel')
     items_file = open(os.path.join(PROJECT_ROOT, 'items.csv'), 'r')
-    items = csv.DictReader(items_file, dialect='excel')
     
-    output_file = open(os.path.join(PROJECT_ROOT, 'output.iif'), 'w')
-
+    square = SquareReader(transactions_file, items_file)
+	
     # This is the name of the QuickBooks checking account
     account = "Square"
 
@@ -63,10 +113,15 @@ def main():
            + "!SPL	SPLID	TRNSTYPE	DATE	ACCNT	NAME	CLASS	AMOUNT	DOCNUM	MEMO	QNTY	PRICE	INVITEM	TAXABLE\r\n"\
     + "!ENDTRNS\r\n"
 
+    #DEBUG
+    square.importTransactions()
+    square.importItems()
+    square.dumpSql()
+    exit()
+
     output_file.write(head)
 
     trans_template = "TRNS		CASH SALE	{month:02d}/{day:02d}/{year:d}	{till_account}	{customer}	{qb_class}	{total:.2f}	{square_id:s}	{cc_digits:s}	N	N\r\n"
-    item_template = "SPL		CASH SALE	{month:02d}/{day:02d}/{year:d}	{sales_account}		{qb_class}	-{total:.2f}			{qty:d}	{price:.2f}	{item_name:s}	N\r\n"
     trans_footer = "ENDTRNS\r\n"
 
 
@@ -76,10 +131,9 @@ def main():
     cfg_customer = 'PRFM Customers'
     cfg_defaultClass = 'Layers'
 
-    # Initialize a buffer for stray items
-    itemBuffer = list()
-    
     # And here's the part that inserts data into the tempalate
+    item = None
+    prevItem = None
     for trans in transactions:
         try:
             (year, month, day) = map(int,trans['Date'].split('-', 2))
@@ -96,27 +150,43 @@ def main():
             continue
         
         output_file.write(trans_template.format(month=month, day=day, year=year, till_account=cfg_cashAccount, customer=cfg_customer, qb_class=cfg_defaultClass, total=total, square_id=square_id, cc_digits=cc_digits))
+        
+        #DEBUG
+        print "--- Transaction %s ---" % (square_id)
+        #END DEBUG
+        itemsCount = 0
         while True:
-            if len(itemBuffer) > 0:
-                item = itemBuffer.pop()
+            prevItem = item
+            if prevItem is not None:
+                itemsCount += 1
+            print "itemsCount: %d" % itemsCount
+            try:
+                item = items.next()
+            except StopIteration:
+                # TODO: handle case for last item
+                outputItem(prevItem, itemsCount, cfg_defaultSalesAccount, cfg_defaultClass)
+                print "!!! StopIteration !!!"
+                break
+            print "item: %r\nprevItem: %r" % (item,prevItem)
+            if prevItem is None or (item['Payment ID'] == square_id and item['Item Name'] == prevItem['Item Name'] and item['Category Name'] == prevItem['Category Name'] and item['Price'] == prevItem['Price']):
+                print "DUPE!"
+                continue
             else:
                 try:
-                    item = items.next()
-                except StopIteration:
+                    outputItem(prevItem, itemsCount, cfg_defaultSalesAccount, cfg_defaultClass)
+                    prevItem = None
+                    itemsCount = 0
+                except:
+                    error(prevItem)
                     break
-                if item['Payment ID'] != square_id:
-                    itemBuffer.append(item)
-                    break
-            try:
-                item_total = float(item['Price'].lstrip('$')) - float(item['Discount'].lstrip('$'))
-            except:
-                error(item)
+                
+            if item['Payment ID'] != square_id:
                 break
             
-            # TODO: count quantities
-            output_file.write(item_template.format(month=month, day=day, year=year, sales_account=cfg_defaultSalesAccount, qb_class=cfg_defaultClass, total=item_total, qty=1, price=item_total, item_name=item['Item Name']))
-        
         output_file.write(trans_footer)
-
+        #DEBUG
+        print "--- END Transaction ---"
+        #END DEBUG
+        
 if __name__ == '__main__':
     main()
